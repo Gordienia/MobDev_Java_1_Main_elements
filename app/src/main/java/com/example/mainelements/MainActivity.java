374de90cb450b76ffd4d82b8747ba2b62861186f
koroteevmv <koroteevmv@gmail.com>
2019-03-28 16:21:51 +0400

    List selection handler added

--- app/src/main/java/com/example/mainelements/MainActivity.java
+++ app/src/main/java/com/example/mainelements/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mainelements;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +13,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        AdapterView.OnItemClickListener{
 
     TextView mainTextView;
     Button mainButton;
@@ -39,12 +42,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 android.R.layout.simple_list_item_1,
                 mNameList);
         mainListView.setAdapter(mArrayAdapter);
+        mainListView.setOnItemClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         mNameList.add(mainEditText.getText().toString());
         mArrayAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("omg android", position + ": " + mNameList.get(position));
+        mainTextView.setText(mNameList.get(position).toString()
+                + " is learning Android development!");
     }
 
 
